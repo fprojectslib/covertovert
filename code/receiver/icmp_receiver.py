c24f644056262d0310895cd37ca1c49cc61b383c
fprojectslib <162815486+fprojectslib@users.noreply.github.com>
2024-10-31 21:52:58 +0300

    icmp_receiever and icmp_sender

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,6 @@
-import scapy
+from scapy.all import sniff, ICMP
+
 
 # Implement your ICMP receiver here
+if __name__ == "__main__":
+    sniff(filter="icmp", prn=lambda packet: packet.show() if packet.haslayer(ICMP) and packet[ICMP].type == 8 else None, count = 1)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,7 @@
-import scapy
+# import scapy
+from scapy.all import IP, ICMP, send
 
 # Implement your ICMP sender here
+packet = IP(dst = "receiver", ttl = 1) / ICMP()
+if __name__ == "__main__":
+    send(packet)
